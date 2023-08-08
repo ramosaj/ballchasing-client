1cf82cde662890a8d34071f7ab49793544ed3876
ramosaj1 <anthonyjoeramos@gmail.com>
2023-08-08 13:13:02 -0500

    added tests

--- ballchasing_client/extract.py
+++ ballchasing_client/extract.py
@@ -11,7 +11,7 @@ class Extractor:
             end=None,
             before_delta = None,
             after_delta=None,
-            freq = 'm',
+            freq = 'M',
             detailed = False
         ):
 
@@ -29,38 +29,34 @@ class Extractor:
             raise Exception("Acceptable combinations of start/end: (start,end), (end,before_delta), (start,after_delta)")
         
         self.player_id = player_id
-        self.date_range = pd.date_range(start=self.start,end=self.end,freq=freq,unit='s').values
+        self.date_range = pd.date_range(start=self.start,end=self.end,freq=freq,unit='s',inclusive='left').values
         self.detailed = detailed
     
 
     def extract_date_range(self):
-        for i,x in enumerate(self.date_range,start=1):
+        result = []
+        for i in range(1,len(self.date_range)):
             after = self.date_range[i-1]
             before = self.date_range[i]
             client = Replays(self.player_id,after=after,before=before)
             result = []
-            try:
-                replays = client.request_records()
-                if self.detailed:
-                    detailed_replays = self.fetch_detailed_many(self, replays)
-                    result += detailed_replays 
-                else: 
-                    result+= replays
+            replays = client.request_records()
+            if self.detailed:
+                detailed_replays = self.fetch_detailed_many(replays)
+                result += detailed_replays 
+            else: 
+                result+= replays
                     
-            except Exception as e:
-                print("Rate limited by ballchasing")
+        
+        return result
+        
     
-    def fetch_detailed_many(self,replay_id=None,replays=None):
+    def fetch_detailed_many(self,replays):
         detailed_replays = []
-        if replays: 
-            for replay in replays:
-                detailed_replay = self.fetch_detailed_one(replay['id'])
-                detailed_replays.append(detailed_replay)
-
-            return detailed_replays
-        elif replay_id:
-            detailed_replay = self.fetch_detailed_one(replay_id)
-            return detailed_replay
+        for replay in replays:
+            detailed_replay = self.fetch_detailed_one(replay['id'])
+            detailed_replays.append(detailed_replay)
+        return detailed_replays
             
     def fetch_detailed_one(self,replay_id):
         obj = DetailedReplays(replay_id)
--- tests/test_extract.py
+++ tests/test_extract.py
@@ -5,23 +5,14 @@ from dateutil.relativedelta import relativedelta
 import pandas as pd
 
 
-class MockReplays:
-
-    def request_records(self):
-            return [{"id":"1234"},{"id":"12345"}]
-
-
-class MockDetailedReplays:
-     def request_records(self):
-            return {"id":"12345"}
-     
-
-
-### Replay.fetch_records() to return list
-
-def test_init():
+def setup_arguments():
     start = datetime(year=2023,month=2,day=1)
-    after_delta = relativedelta(months = 1)
+    after_delta = relativedelta(months = 2)
+    return start, after_delta
+
+   
+def test_init():
+    start,after_delta = setup_arguments()
     extract = Extractor(start=start,after_delta=after_delta,player_id="Retals")
 
     assert(extract.start == start)
@@ -33,34 +24,46 @@ def test_fetch_detailed_one(monkeypatch):
     def mock_request_records(*args,**kwargs):
         return {"id":"1234"}
     monkeypatch.setattr(DetailedReplays,"request_records",mock_request_records)
-    start = datetime(year=2023,month=2,day=1)
-    after_delta = relativedelta(month = 1)
+    start,after_delta = setup_arguments()
     extract = Extractor(start=start,after_delta=after_delta,player_id="Retals")
     assert(extract.fetch_detailed_one("12345") == {"id":"1234"})
 
 
 def test_fetch_detailed_many(monkeypatch):
     def mock_request_records(*args,**kwargs):
-        return [{"id":"1234"},{"id":"12345"}]
+        return {"id":"12345"}
 
-    replays = [{"id":"1234"},{"id":"12345"}]
-    monkeypatch.setattr(Replays,"request_records",mock_request_records)
-    start = datetime(year=2023,month=2,day=1)
-    after_delta = relativedelta(month = 1)
+    replays = [{"id":"12345"}]
+    monkeypatch.setattr(DetailedReplays,"request_records",mock_request_records)
+    start,after_delta = setup_arguments()
     extract = Extractor(start=start,after_delta=after_delta,player_id="Retals")
-    print(extract.fetch_detailed_many(replays))
-    assert( {"id":"12345"} == extract.fetch_detailed_many(replays)[0])
-    assert( {"id":"1234"} == extract.fetch_detailed_many(replays)[1])
+    assert(extract.fetch_detailed_many(replays)[0]['id'] == '12345')
 
     
-
 def test_extract_date_range(monkeypatch):
+    def mock_request_records(*args,**kwargs):
+        return [{"id":"1234"},{"id":"12345"}]
+
+    monkeypatch.setattr(Replays,"request_records",mock_request_records)
+    start,after_delta = setup_arguments()
+    extract = Extractor(start=start,after_delta=after_delta,player_id="Retals")
+    print(extract.date_range)
+    assert(extract.extract_date_range()[0]['id'] == "1234")
+
+
+def test_detailed_extract_date_range(monkeypatch):
     def mock_fetch_detailed(*args,**kwargs):
         return {"id":"1234"}
+    
+    def mock_request_records(*args,**kwargs):
+        return [{"id":"1234"},{"id":"12345"}]
+
+    monkeypatch.setattr(Replays,"request_records",mock_request_records)
     monkeypatch.setattr(Extractor,"fetch_detailed_one",mock_fetch_detailed)
-    start = datetime(year=2023,month=2,day=1)
-    after_delta = relativedelta(month = 1)
-    extract = Extractor(start=start,after_delta=after_delta,player_id="Retals")
+
+    start,after_delta = setup_arguments()
+    extract = Extractor(start=start,after_delta=after_delta,player_id="Retals",detailed=True)
+    assert(extract.extract_date_range())
 
 
 
